8930dd8ca200aebab1ab8cdb24b0ee48cff57a34
Fenix <Fenixisgod>
2023-09-09 20:26:41 +0900

    夢小説　正気に戻りたい

---
+++ js/yumesyousetu.js
@@ -0,0 +1,14 @@
+document.getElementById("GOTOHELL").onclick = function () {
+  let formname = document.getElementById("name").value;
+  let formgiftname = document.getElementById("giftName").value;
+
+  var name = formname ? formname : "##NAME1##"
+  var giftName = formgiftname ? formgiftname : "##NAME2##"
+
+  var str = document.getElementById("honbun").innerHTML;
+  str = str.replace("##NAME1##", name);
+  str = str.replace("##NAME2##", giftName);
+  document.getElementById("honbun").innerHTML = str;
+  str.style.visibility = "visible"
+
+}
